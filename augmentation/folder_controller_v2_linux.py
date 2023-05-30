d4caff26319dedbcb67c321abe25512f8d523541
woorej <wjk5848@gmail.com>
2023-05-30 11:14:45 +0900

    folder_controller_v2_linux

--- augmentation/folder_controller_v2_linux.py
+++ augmentation/folder_controller_v2_linux.py
@@ -1,4 +1,5 @@
 import shutil
+from typing import List
 import os
 import collections
 from tqdm import tqdm
@@ -8,7 +9,7 @@ target.txt를 이용해 폴더만드는 부분
 """
 def make_folder_structure(text_file_name: str) -> set:
     folder_maps : list = []
-    with open("target.txt", "r") as f: 
+    with open(text_file_name, "r") as f: 
         readlines = f.readlines()
         for readline in readlines :
             # txt파일에서 엔터키를 제거하고 리스트로 만든다.
@@ -22,14 +23,17 @@ def make_folder_structure(text_file_name: str) -> set:
             folder_maps.append(line)
     # 등록번호 아래에 AI, 크롤링 이라는 폴더를 만들기 위해 리스트를 만든다.
     collections=['AI','크롤링']
+    print(folder_maps)
     # 대분류/소분류 경로형태로 가지고 있는 집합을만듬
     category_sets = set()
     for folder_map in tqdm(folder_maps, desc="make_folder_structure", ncols=110, ascii = ' =') :
         main_category = folder_map[0] # 대분류를 저장
         sub_category = folder_map[1] # 소분류를 저장
         category_sets.add(main_category+"/"+sub_category) # 대분류/소분류 저장
-        design_number_category = folder_map[2] # 등록번호 저장
-        
+        try :
+            design_number_category = folder_map[2] # 등록번호 저장
+        except IndexError :
+            raise "Target.txt파일이 비어있는 등록번호가 있습니다."
         main_category_dir = os.path.abspath(main_category) # 대분류를 절대경로로 변경
         sub_category_dir = os.path.abspath(os.path.join(main_category, sub_category)) # 대분류/소분류를 join하고 절대경로로 저장
         design_number_category_dir = os.path.abspath(os.path.join(main_category, sub_category, design_number_category)) # 대분류/소분류/등록번호를 join하고 절대경로로 저장
@@ -44,15 +48,15 @@ def make_folder_structure(text_file_name: str) -> set:
 """
 만들어둔 폴더에 파일을 이동해주는 부분
 """
-import time
-def parse_file(category_sets: set, root_path: str) -> None :
+def parse_file(category_sets: set, root_path: str, save_in: str) -> None :
     # 모든 등록번호가 저장되어 정리되지 않은 폴더
     # 절대경로로 변경해준다.
-    root_abspath = os.path.abspath(root_path) 
+    moved_file_lists: List[str] = []
+    root_abspath = os.path.abspath(root_path)  # 미분류_검수완료_aug의 절대경로
     for root, dirs, files in tqdm(os.walk(root_abspath), desc="parse_file", ncols=110, ascii = ' =', position=0) :
         for file in files :
             # 모든 파일들을 싹다 읽어온다.
-            file_path = os.path.join(root, file) 
+            file_path = os.path.join(root, file)
             for category_set in tqdm(category_sets, ncols=110, ascii = ' =', position=1, leave=False) : 
                 # 미분류라고 표시된 문자열을 대분류/소분류 replace한 절대경로를 저장
                 parsed_file_path = file_path.replace(root_path, category_set) 
@@ -61,21 +65,22 @@ def parse_file(category_sets: set, root_path: str) -> None :
                 parsed_file_path = parsed_file_path.split('/')
                 parsed_file_path[-2] = re.sub(r'\W+','',parsed_file_path[-2])
                 parsed_file_path = '/'.join(parsed_file_path)
-                path = os.path.join("/".join(parsed_file_path.split("/")[:-1]),"크롤링") 
+                path = os.path.join("/".join(parsed_file_path.split("/")[:-1]), save_in) 
                 # 경로를 붙여주기 위해 파일이름을 저장한다.
                 name = parsed_file_path.split("/")[-1]
                 # 크롤링 경로를 붙여주고 파일의 이름을 합쳐서 최종 절대경로를 만든다.
                 parsed_file_crawling_path = os.path.join(path, name) 
                 # 사전에 만든 파일 파일 경로가 존재하면 이동하고 그렇지 않으면 넘긴다.
-                if os.path.exists(path) : 
+                if os.path.exists(path) :
+                    moved_file_lists.append(file_path)
                     shutil.copyfile(file_path, parsed_file_crawling_path)
                 #else :
                     #print(f"Check File: {file_path}")
+    return moved_file_lists
                     
 """
 각 등록번호의 이미지 개수를 출력하는 부분
 """
-import time
 def file_counter(category_sets: set) -> None :
     #print(category_sets) # {'IT/USB 메모리 스틱', '생활가전/밥솥', 'IT/헤드셋', '패션잡화/바지'}  
     file_counter_list: list = []
@@ -93,7 +98,8 @@ def file_counter(category_sets: set) -> None :
                 file_counter_list.append(dist)
                 # 갯수 카운팅
                 counter = collections.Counter(file_counter_list)    
-          
+        
+    total_num = 0
     # 기존 텍스트 읽어와서
     with open("target.txt", "r") as f: 
         # 한줄씩 읽고
@@ -115,10 +121,27 @@ def file_counter(category_sets: set) -> None :
             # 숫자만 나오게해서 복사 붙여넣기를 편하게한다
             with open("counter_without_target.txt","a") as c :
                 c.write(f"{str(counter[design_number])}\n")
+                total_num +=counter[design_number]   
+    
+    print(f"TOTAl NUMBER OF IMAGE: {total_num}")
+
+def show_not_parsed_file(root_path: str, moved_file_lists: List[str]) :
+    not_moved_file_lists : List[str] = []
+    root_abspath = os.path.abspath(root_path)
+    print("NUMBER OF FILE NOT MOVED: ", end='')
+    for root, dirs, files in os.walk(root_abspath) :
+        for file in files  :
+            file_path = os.path.join(root, file)
+            if file_path not in moved_file_lists :
+                not_moved_file_lists.append(file_path)
+                with open("file_not_moved.txt", 'a') as f :
+                    f.write(f"{file_path}\n")
+    print(len(not_moved_file_lists))
                 
 
 if __name__=="__main__" :
     category_sets = make_folder_structure(text_file_name="target.txt") # 폴더구조 생성
-    parse_file(category_sets=category_sets, root_path="미분류_검수완료_aug") # root_path는 폴더구조를 재정의하고자하는 src
+    moved_file_lists = parse_file(category_sets=category_sets, root_path="미분류_검수완료_aug", save_in="크롤링") # root_path는 폴더구조를 재정의하고자하는 src
+    #parse_file(category_sets=category_sets, root_path="미분류_검수완료_aug_AI", save_in="AI") # root_path는 폴더구조를 재정의하고자하는 src
     file_counter(category_sets=category_sets) # 파일 갯수 카운팅
-
+    show_not_parsed_file(root_path="미분류_검수완료_aug", moved_file_lists=moved_file_lists)
